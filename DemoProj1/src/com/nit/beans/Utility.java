155e3d7343a1776db539be43ee4067b28e42bc73
sareesh.dell <sareesh@gmail.com>
2024-09-16 16:13:52 +0530

    DemoProj Commited by Sari

---
+++ DemoProj1/src/com/nit/beans/Utility.java
@@ -0,0 +1,14 @@
+package com.nit.beans;
+
+import java.sql.Date;
+
+public class Utility {
+	public static void main(String[] args) {
+		System.out.println("Date method called");
+	}
+	public Date getSysDate() {
+		
+		return new Date(0);
+	}
+
+}

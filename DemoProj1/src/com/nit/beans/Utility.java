b8409fb192412f1e48843abaf6b06ee7c7ef2af1
sareesh.dell <sareesh@gmail.com>
2024-09-16 19:31:22 +0530

    TL commit master

--- DemoProj1/src/com/nit/beans/Utility.java
+++ DemoProj1/src/com/nit/beans/Utility.java
@@ -17,8 +17,10 @@ public class Utility {
 		System.out.println("utility m1 method creatd....1234:");
   
        System.out.println("12345abcd");
-
-
+	}
+	
+	public void F1() {
+		System.out.println("f1()");
 	}
 
 }
